c00578c437dd96e282c9b6fae296f254eef7d103
lolilolicon <lolilolicon@gmail.com>
2009-11-23 20:54:12 +0800

    fixed valueEVcodec() valueEVformat() (stupid mistake &>/dev/null); added more debug info

--- xrectsel.c
+++ xrectsel.c
@@ -24,7 +24,10 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 */
-
+/*
+Description: drag a rectangle area on screen and it prints geometry wxh+x+y
+Compile: gcc  xrectsel.c -lX11 -o xrectsel
+*/
 
 #include<stdio.h>
 #include<stdlib.h>
